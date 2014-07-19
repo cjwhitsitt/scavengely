de35956c31fe72d503993666cabe08045b394bd0
Steven Palomino <steven.palomino@hallmark.com>
2014-07-19 12:24:34 -0500

    UI changes and file updates

---
+++ Scavengely/ActivityViewController.m
@@ -0,0 +1,38 @@
+//
+//  ActivityViewController.m
+//  Scavengely
+//
+//  Created by Steven Palomino on 7/19/2014.
+//  Copyright (c) 2014 Jay Whitsitt. All rights reserved.
+//
+
+#import "ActivityViewController.h"
+
+@interface ActivityViewController ()
+
+@end
+
+@implementation ActivityViewController
+
+
+
+- (void)viewDidLoad
+{
+    [self.navigationController setNavigationBarHidden:NO];
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
