d797bfaf27228c29531a803e884700d194208111
Steven Palomino <steven.palomino@hallmark.com>
2014-07-19 20:52:49 -0500

    Storyboard changes for MissionViewController

---
+++ Scavengely/MissionViewController.h
@@ -0,0 +1,16 @@
+//
+//  MissionViewController.h
+//  Scavengely
+//
+//  Created by Steven Palomino on 7/19/2014.
+//  Copyright (c) 2014 Jay Whitsitt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MissionViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *missionImage;
+@property (weak, nonatomic) IBOutlet UILabel *missionPrompt;
+@property (weak, nonatomic) IBOutlet UIButton *missionButton;
+
+@end
---
+++ Scavengely/MissionViewController.m
@@ -0,0 +1,49 @@
+//
+//  MissionViewController.m
+//  Scavengely
+//
+//  Created by Steven Palomino on 7/19/2014.
+//  Copyright (c) 2014 Jay Whitsitt. All rights reserved.
+//
+
+#import "MissionViewController.h"
+
+@interface MissionViewController ()
+
+@end
+
+@implementation MissionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
