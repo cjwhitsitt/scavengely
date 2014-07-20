3ad3f146f25ee2c520813fc60d2780dc8df85134
Jay Whitsitt <jay.whitsitt@gmail.com>
2014-07-19 23:30:11 -0500

    AudioCaptureViewController

---
+++ Scavengely/AudioCaptureViewController.m
@@ -0,0 +1,96 @@
+//
+//  AudioCaptureViewController.m
+//  Scavengely
+//
+//  Created by Jay Whitsitt on 7/19/2014.
+//  Copyright (c) 2014 Jay Whitsitt. All rights reserved.
+//
+
+#import <MobileCoreServices/MobileCoreServices.h>
+#import "AudioCaptureViewController.h"
+
+@interface AudioCaptureViewController ()
+
+@property (strong, nonatomic) UIImagePickerController *imagePickerController;
+@property (strong, nonatomic) IBOutlet UIButton *imageCaptureButton;
+
+@end
+
+@implementation AudioCaptureViewController
+
+#pragma mark custom methods
+
+#pragma mark UIViewController methods
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    [self startCameraControllerFromViewController:self usingDelegate:self];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark UIImagePickerControllerDelegate methods
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+    [picker popViewControllerAnimated:YES];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [[picker parentViewController] dismissModalViewControllerAnimated: YES];
+}
+
+- (BOOL)startCameraControllerFromViewController:(UIViewController*) controller
+                                  usingDelegate:(id <UIImagePickerControllerDelegate, UINavigationControllerDelegate>) delegate {
+    
+    if (([UIImagePickerController isSourceTypeAvailable: UIImagePickerControllerSourceTypeCamera] == NO) || (delegate == nil) || (controller == nil))
+        return NO;
+    
+    
+    UIImagePickerController *cameraUI = [[UIImagePickerController alloc] init];
+    cameraUI.sourceType = UIImagePickerControllerSourceTypeCamera;
+    
+    // Displays a control that allows the user to choose picture or
+    // movie capture, if both are available:
+    cameraUI.mediaTypes = [[NSArray alloc] initWithObjects: (NSString *) kUTTypeImage, nil];
+    
+    // Hides the controls for moving & scaling pictures, or for
+    // trimming movies. To instead show the controls, use YES.
+    cameraUI.allowsEditing = NO;
+    
+    cameraUI.delegate = delegate;
+    
+    [controller presentModalViewController: cameraUI animated: YES];
+    return YES;
+}
+
+@end
